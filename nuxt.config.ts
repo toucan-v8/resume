30735e1e6d6e935179365e279d542efa532bac03
toucan-v8 <toucan305@gmail.com>
2025-05-26 21:56:26 +0300

    Add and configure nuxtjs/sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxt/fonts"],
+  modules: ["@nuxt/image", "@nuxt/fonts", "@nuxtjs/sitemap"],
   css: ["~/assets/main.css"],
   ssr: true,
   app: {
     baseURL: "/resume/",
+  },
+  site: {
+    url: "https://toucan-v8.github.io/",
+  },
+  sitemap: {
+    xsl: false,
+    exclude: ["/resume/resume"],
   },
 });
